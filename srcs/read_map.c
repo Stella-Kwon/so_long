42e8158c113a7c98711c1bc54ec1aff1d9b661b7
Stella-Kwon <60865769+Stella-Kwon@users.noreply.github.com>
2024-05-23 13:54:10 +0300

    Update read_map.c

--- srcs/read_map.c
+++ srcs/read_map.c
@@ -14,35 +14,43 @@
 
 void	each_line(t_maps *map, int fd)
 {
+	// Read each line and concatenate it into map->map_oneline
 	while (map->line)
 	{
-		map->map_oneline = ft_strjoin(map->map_oneline, map->line);
-		if (!map->map_oneline)
+		char *tmp = ft_strjoin(map->map_oneline, map->line);
+		if (!tmp)
 			errors("Map reading malloc failed", map);
-		map->line = get_next_line(fd);
-		if (map->line)
-		{
-			map->height++;
-			if (ft_strncmp(map->line, "\n", 1) == 0)
-				errors("Map has empty line in the middle", map);
-		}
+		free_null(&map->map_oneline); // Free the previous concatenated string -> "" tobe null
+		map->map_oneline = tmp; // Update with the new concatenated string -> so oneline will direct to tmp address and later it will free the tmp as well
+		map->height++;
+		if (ft_strncmp(map->line, "\n", 1) == 0)
+			errors("Map has empty line in the middle", map);
+		free_null(&map->line); // Free the current line after processing
+		map->line = get_next_line(fd); // Read the next line
 	}
+	// Check for an empty line at the end of the map
 	if (map->map_oneline[ft_strlen(map->map_oneline) - 1] == '\n')
 		errors("Empty line at the end of the map", map);
+	// Split the concatenated string into an array of strings
 	map->map = ft_split(map->map_oneline, '\n');
 	if (!map->map)
-		errors("Split error in the each_line function Or empty map", map);
+		errors("Split error in the each_line function or empty map", map);
 }
 
 int	read_map(char *file, t_maps *map)
 {
-	int		fd;
+	int	fd;
 
 	fd = open(file, O_RDONLY);
 	if (fd < 0)
 		errors("Open the map failed.", map);
 	map->line = get_next_line(fd);
+	map->map_oneline = ft_strdup("");// Initialize map_oneline with an empty string
+	if (!map->map_oneline)
+		errors("Initial map_oneline malloc failed", map);
 	map->width = ft_strlen(map->line) - 2;
 	each_line(map, fd);
+	close(fd);
 	return (SUCCESS);
 }
+
